f39605147f7f0dc22c8af50e6bd34f7792b695a8
Aleksandra Litvinenko <alexandralit09022005@gmail.com>
2026-03-23 21:52:12 +0300

    add struct of task

---
+++ models/task.go
@@ -0,0 +1,14 @@
+package models
+
+type Task struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Text      string `json:"text"`
+	Completed bool   `json:"completed"`
+}
+
+type NewTask struct {
+	Title     string `json:"title"`
+	Text      string `json:"text"`
+	Completed bool   `json:"completed"`
+}
