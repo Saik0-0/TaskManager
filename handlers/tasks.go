1a98efc2c0eaf10c3bfc529b85b05761414a4039
Aleksandra Litvinenko <alexandralit09022005@gmail.com>
2026-03-23 21:02:36 +0300

    add method GET in /tasks handler

--- handlers/tasks.go
+++ handlers/tasks.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Saik0-0/TaskManager/models"
 	"github.com/Saik0-0/TaskManager/storage"
 	"net/http"
+	"strconv"
 )
 
 type Server struct {
@@ -32,6 +33,54 @@ func (server *Server) TasksHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+	case http.MethodGet:
+		query := r.URL.Query()
+		filter := query.Get("filter")
+		response := storage.Response{}
+		if filter != "" {
+			response = server.Store.GetAllTasksFiltered(filter)
+		} else {
+			response = server.Store.GetAllTasks()
+		}
+
+		offset := 0
+		limit := response.Total
+
+		offsetString := query.Get("offset")
+		if offsetString != "" {
+			o, err := strconv.Atoi(offsetString)
+			if err != nil {
+				http.Error(w, "Invalid offset: must be integer", http.StatusBadRequest)
+			}
+			offset = o
+		}
+
+		limitString := query.Get("limit")
+		if limitString != "" {
+			l, err := strconv.Atoi(limitString)
+			if err != nil {
+				http.Error(w, "Invalid limit: must be integer", http.StatusBadRequest)
+			}
+			limit = l
+		}
+
+		if offset > response.Total {
+			offset = response.Total
+		}
+		end := offset + limit
+		if end > response.Total {
+			end = response.Total
+		}
+
+		response.Tasks = response.Tasks[offset:end]
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			fmt.Println("Encoding error: ", err)
+			return
+		}
+		
 	default:
 		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
 		return
