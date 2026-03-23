186c1fb11df161b93e4008feaceea2c8fbb6f4d4
Aleksandra Litvinenko <alexandralit09022005@gmail.com>
2026-03-23 18:58:43 +0300

    Add main file

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Saik0-0/TaskManager/handlers"
+	"github.com/Saik0-0/TaskManager/models"
+	"github.com/Saik0-0/TaskManager/storage"
+	"net/http"
+)
+
+func main() {
+	taskStore := storage.TaskStore{
+		Tasks: make(map[int]models.Task),
+	}
+
+	server := handlers.Server{
+		Store: &taskStore,
+	}
+
+	http.HandleFunc("/tasks", server.TasksHandler)
+
+	if err := http.ListenAndServe(":9092", nil); err != nil {
+		fmt.Println("Listening error: ", err)
+	}
+}
