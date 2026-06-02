ae26480cd54fd3d407e4f9b7bffadad92a1baf29
Aleksandra Litvinenko <alexandralit09022005@gmail.com>
2026-06-02 21:48:04 +0300

    add simple connection to database

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/Saik0-0/TaskManager
 
-go 1.23.3
+go 1.26.1
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.9.2 // indirect
+	golang.org/x/text v0.29.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/Saik0-0/TaskManager/feature_postgresql"
 	"github.com/Saik0-0/TaskManager/handlers"
 	"github.com/Saik0-0/TaskManager/models"
 	"github.com/Saik0-0/TaskManager/storage"
@@ -9,6 +10,7 @@ import (
 )
 
 func main() {
+	feature_postgresql.CheckConnection()
 	taskStore := storage.TaskStore{
 		Tasks: make(map[int]models.Task),
 	}
